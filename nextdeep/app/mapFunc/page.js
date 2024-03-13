acb3a2fd6789efb215478ef9302730e0f36d13d4
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-03-13 16:59:25 +0500

    map

--- nextdeep/app/mapFunc/page.js
+++ nextdeep/app/mapFunc/page.js
@@ -1,12 +1,17 @@
+"use client";
 const MapFunc = () => {
   let arr = [
     { Name: "Ali", RollNo: "32", Section: "A" },
     { Name: "Asger", RollNo: "33", Section: "C" },
     { Name: "Fezan", RollNo: "34", Section: "M" },
   ];
+  const deleteHandler = () => {
+    arr.shift();
+    console.log(arr);
+  };
   return (
     <div>
-      {arr.map((item) => {
+      {/* {arr.map((item) => {
         return (
           <>
             <h1 className="text-center"> Hellow - {item.Name}</h1>
@@ -15,7 +20,31 @@ const MapFunc = () => {
             <hr />
           </>
         );
-      })}
+      })} */}
+      <table>
+        <tr>
+          <th>StudentName</th>
+          <th>RollNo</th>
+          <th>Section</th>
+        </tr>
+        {arr.map((item, i) => {
+          return (
+            <tr key={i}>
+              <td>{item.Name}</td>
+              <td>{item.RollNo}</td>
+              <td>{item.Section}</td>
+              <td>
+                <button
+                  className="bg-gray-300 rounded-md"
+                  onClick={deleteHandler}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 };
