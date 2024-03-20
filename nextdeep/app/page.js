cb238d127b36fbd0fa41c2796d54fe77225a285b
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-03-20 22:08:56 +0500

    commit

--- nextdeep/app/page.js
+++ nextdeep/app/page.js
@@ -1,14 +1,18 @@
-import State from "./ifState/page";
+// import State from "./ifState/page";
 import MapFunc from "./mapFunc/page";
 import Counting from "./simpleCounter/page";
 
 export default function Home() {
   return (
-    <>
-      <h1>Hellow-World</h1>
-      <Counting />
-      <State />
-      <MapFunc />
-    </>
+    <div className="p-4 grid grid-cols-1 md:grid-cols-2 mt-10">
+      <div className="mt-20">
+        <Counting />
+      </div>
+
+      {/* <State /> */}
+      <div>
+        <MapFunc />
+      </div>
+    </div>
   );
 }
--- nextdeep/app/simpleCounter/page.js
+++ nextdeep/app/simpleCounter/page.js
@@ -42,15 +42,14 @@ const Counting = () => {
             Clear
           </button>
         )}
-        {/* <button
-          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${className}`}
-          onClick={onClick}
-        >
-          Botton
-        </button> */}
-        <div>
-          <h1>Only for practice</h1>
-          <h2>Deep study</h2>
+
+        <div className="mt-8 mb-8 grid grid-cols-1 space-y-4 md:grid-cols-2">
+          <div className="mt-4">
+            <h1>Only for practice</h1>
+          </div>
+          <div>
+            <h2>Deep study</h2>
+          </div>
         </div>
       </div>
     </div>
