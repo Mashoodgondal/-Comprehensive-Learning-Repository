cb3431988763f0d30ee50ba3e695d2014fa1bd75
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-03-24 22:10:45 +0500

    target value

--- nextdeep/app/targetValue/page.js
+++ nextdeep/app/targetValue/page.js
@@ -8,14 +8,13 @@ const Getvalue = () => {
   };
   return (
     <div>
-      {" "}
       <div className="m-4 text-center ">
         <input
-          className="border border-gray-600 rounded"
+          className="border border-gray-600 rounded focuc:outline-blue-400"
           type="text"
           onChange={changehandler}
         ></input>
-        <h2>output:{name}</h2>
+        <h2 className="mt-2 font-bolt text-gray-500">output:{name}</h2>
       </div>
     </div>
   );
