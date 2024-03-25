65d3435cc6fe56831846aa8a25ec26ab1ab3e1fe
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-03-25 22:42:36 +0500

    commit

--- nextdeep/app/targetValue/page.js
+++ nextdeep/app/targetValue/page.js
@@ -11,27 +11,33 @@ const Getvalue = () => {
   return (
     <div>
       <div className="m-4 text-center ">
-        <input
-          className="border border-gray-600 rounded focuc:outline-blue-400"
-          type="text"
-          placeholder="Enter name"
-          onChange={(e) => setname(e.target.value)}
-        ></input>
-        <input
-          className="border border-gray-600 rounded focuc:outline-blue-400"
-          type="email"
-          placeholder="Enter email"
-          onChange={(e) => setemail(e.target.value)}
-        ></input>
-        <input
-          className="border border-gray-600 rounded focuc:outline-blue-400"
-          type="number"
-          placeholder="Enter phone number"
-          onChange={(e) => setphone(e.target.value)}
-        ></input>
-        <h2 className="mt-2mt-2 font-bolt text-gray-500">Enter Name:{name}</h2>
-        <h2 className=" font-bolt text-gray-500">Enter Email:{email}</h2>
-        <h2 className="mt-2 font-bolt text-gray-500">Enetr Phone:{phone}</h2>
+        <div className="m-4 flex flex-wrap space-x-4">
+          <input
+            className="border border-gray-600   rounded focuc:outline-blue-400"
+            type="text"
+            placeholder="Enter name"
+            onChange={(e) => setname(e.target.value)}
+          ></input>
+          <input
+            className="border border-gray-600 rounded focuc:outline-blue-400"
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setemail(e.target.value)}
+          ></input>
+          <input
+            className="border border-gray-600 rounded focuc:outline-blue-400"
+            type="number"
+            placeholder="Enter phone number"
+            onChange={(e) => setphone(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <h2 className="mt-2mt-2 font-bolt text-gray-800">
+            Enter Name:{name}
+          </h2>
+          <h2 className=" font-bolt text-gray-800">Enter Email:{email}</h2>
+          <h2 className="mt-2 font-bolt text-gray-800">Enetr Phone:{phone}</h2>
+        </div>
       </div>
     </div>
   );
