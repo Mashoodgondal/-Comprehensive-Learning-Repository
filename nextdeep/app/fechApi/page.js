ac3d25de3af4e7d287dc6d4cb2a90b1657532525
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-03-27 22:14:09 +0500

    commit

--- nextdeep/app/fechApi/page.js
+++ nextdeep/app/fechApi/page.js
@@ -6,6 +6,11 @@ const FechApi = () => {
       <h1 className="mt-6 text-3xl text-indigo-600 text-center font-bold">
         Fech Data From API
       </h1>
+      <p className="text-center mt-4">This is use to fech data from API </p>
+      <p className="text-center mt-4 mt-4 mb-4">
+        This is use to fech data from API{" "}
+      </p>
+      <p className="text-center mt-4">This is use to fech data from API </p>
     </div>
   );
 };
