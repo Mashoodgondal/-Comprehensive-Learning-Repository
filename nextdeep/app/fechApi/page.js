432b90b4a7db62e5d26bfaf77cd393e6ba260c9c
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-03-28 17:38:06 +0500

    commit

--- nextdeep/app/fechApi/page.js
+++ nextdeep/app/fechApi/page.js
@@ -16,11 +16,11 @@ const FechApi = () => {
       </h1>
       <button
         onClick={fetchitems}
-        className="bg-gray-400 p-1 rounded-md hove:bg-gray-500 m-4"
+        className="bg-gray-400 p-1 rounded-md hover:bg-gray-300 m-4"
       >
         fetch items
       </button>
-      {products ? (
+      {products && (
         <table className="border-collapse border border-gray-400">
           <thead>
             <tr>
@@ -51,65 +51,9 @@ const FechApi = () => {
             ))}
           </tbody>
         </table>
-      ) : (
-        <div>Products not found</div>
       )}
     </div>
   );
 };
 
 export default FechApi;
-
-// import { useState } from "react";
-
-// const FechApi = () => {
-//   const [products, setProducts] = useState([]);
-
-//   const fetchItems = async () => {
-//     try {
-//       const response = await fetch("https://fakestoreapi.com/products");
-//       const productsData = await response.json();
-//       setProducts(productsData);
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//     }
-//   };
-
-//   return (
-//     <div className="m-6 p-4">
-//       <h1 className="text-3xl text-indigo-600 text-center font-bold">
-//         Fetch Data From API
-//       </h1>
-//       <button onClick={fetchItems}>Fetch Items</button>
-
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>id</th>
-//             <th>img</th>
-//             <th>title</th>
-//             <th>price</th>
-//             <th>category</th>
-//             <th>description</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {products.map((product) => (
-//             <tr key={product.id}>
-//               <td>{product.id}</td>
-//               <td>
-//                 <img src={product.image} width={40} alt="" />
-//               </td>
-//               <td>{product.title}</td>
-//               <td>{product.price}</td>
-//               <td>{product.category}</td>
-//               <td>{product.description.slice(0, 20)}...</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// };
-
-// export default FechApi;
