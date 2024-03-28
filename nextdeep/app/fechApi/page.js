6b4949fc3a5f289870c1dcc777aa236802fe7997
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-03-28 17:28:37 +0500

    commit

--- nextdeep/app/fechApi/page.js
+++ nextdeep/app/fechApi/page.js
@@ -10,35 +10,50 @@ const FechApi = () => {
     console.log(products);
   };
   return (
-    <div className="m-6 p-4">
+    <div className="m-6 p-4 text-center">
       <h1 className="text-3xl text-indigo-600 text-center font-bold">
         Fech Data From API
       </h1>
-      <button onClick={fetchitems}>fetch items</button>
-      <table>
-        <tr>
-          <th>id</th>
-          <th>img</th>
-          <th>title</th>
-          <th>price</th>
-          <th>category</th>
-          <th> description</th>
-        </tr>
-        {products.map((product) => {
-          return (
+      <button
+        onClick={fetchitems}
+        className="bg-gray-400 p-1 rounded-md hove:bg-gray-500 m-4"
+      >
+        fetch items
+      </button>
+      {products ? (
+        <table className="border-collapse border border-gray-400">
+          <thead>
             <tr>
-              <td>{product.id}</td>
-              <td>
-                <img src={product.image} width={40} alt="" />
-              </td>
-              <td>{product.title}</td>
-              <td> {product.price} </td>
-              <td> {product.category} </td>
-              <td> {product.description.slice(0, 20)} </td>
+              <th className="border border-gray-400 p-2">id</th>
+              <th className="border border-gray-400 p-2">img</th>
+              <th className="border border-gray-400 p-2">title</th>
+              <th className="border border-gray-400 p-2">price</th>
+              <th className="border border-gray-400 p-2">category</th>
+              <th className="border border-gray-400 p-2">description</th>
             </tr>
-          );
-        })}
-      </table>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td className="border border-gray-400 p-2">{product.id}</td>
+                <td className="border border-gray-400 p-2">
+                  <img src={product.image} width={40} alt="" />
+                </td>
+                <td className="border border-gray-400 p-2">{product.title}</td>
+                <td className="border border-gray-400 p-2">{product.price}</td>
+                <td className="border border-gray-400 p-2">
+                  {product.category}
+                </td>
+                <td className="border border-gray-400 p-2">
+                  {product.description.slice(0, 20)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div>Products not found</div>
+      )}
     </div>
   );
 };
