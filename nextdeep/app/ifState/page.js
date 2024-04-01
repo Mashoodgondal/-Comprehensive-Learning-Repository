de468a5991f72c7a93d3d7198be2b292f6e99a1d
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-01 22:28:45 +0500

    commit

--- nextdeep/app/ifState/page.js
+++ nextdeep/app/ifState/page.js
@@ -15,9 +15,13 @@ const State = () => {
 
   return (
     <div className="m-5 text-center ">
-      {score >= 60 && <h2>you are passed and grade is: {grade}</h2>}
+      {score >= 60 ? (
+        <h2>you are passed and grade is: {grade}</h2>
+      ) : (
+        <div>you are failed </div>
+      )}
 
-      {score < 60 && <h2>you are failed and grade is: {grade}</h2>}
+      {/* {score < 60 && <h2>you are failed and grade is: {grade}</h2>} */}
     </div>
   );
 };
