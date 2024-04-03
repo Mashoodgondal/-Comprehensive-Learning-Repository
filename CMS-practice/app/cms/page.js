534670831f6ba67ef99a589fb1500b379070fc7a
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-03 22:47:44 +0500

    commit

--- CMS-practice/app/cms/page.js
+++ CMS-practice/app/cms/page.js
@@ -19,6 +19,22 @@ const CmsPractice = () => {
         use for cms practice{" "}
       </h1>
       <p>this work with calling api and install library </p>
+      <h1 className="text-indigo-500 text-center mt-10">
+        use for cms practice{" "}
+      </h1>
+      <p>this work with calling api and install library </p>
+      <h1 className="text-indigo-500 text-center mt-10">
+        use for cms practice{" "}
+      </h1>
+      <p>this work with calling api and install library </p>
+      <h1 className="text-indigo-500 text-center mt-10">
+        use for cms practice{" "}
+      </h1>
+      <p>this work with calling api and install library </p>
+      <h1 className="text-indigo-500 text-center mt-10">
+        use for cms practice{" "}
+      </h1>
+      <p>this work with calling api and install library </p>
     </div>
   );
 };
