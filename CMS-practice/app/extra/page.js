3cd09b907364efee41b81b3d6f046e5f146be137
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-05 22:26:43 +0500

    commit

--- CMS-practice/app/extra/page.js
+++ CMS-practice/app/extra/page.js
@@ -23,6 +23,26 @@ const page = () => {
         use for cms practice{" "}
       </h1>
       <p>this work with calling api and install library </p>
+      <h1 className="text-indigo-500 text-center mt-10">
+        use for cms practice{" "}
+      </h1>
+      <p>this work with calling api and install library </p>
+      <h1 className="text-indigo-500 text-center mt-10">
+        use for cms practice{" "}
+      </h1>
+      <p>this work with calling api and install library </p>
+      <h1 className="text-indigo-500 text-center mt-10">
+        use for cms practice{" "}
+      </h1>
+      <p>this work with calling api and install library </p>
+      <h1 className="text-indigo-500 text-center mt-10">
+        use for cms practice{" "}
+      </h1>
+      <p>this work with calling api and install library </p>
+      <h1 className="text-indigo-500 text-center mt-10">
+        use for cms practice{" "}
+      </h1>
+      <p>this work with calling api and install library </p>
     </div>
   );
 };
