7512442c56ccf1cf3e8a0d626c1ae71033d2a9f2
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-06 17:01:17 +0500

    commit

--- CMS-practice/app/extra/page.js
+++ CMS-practice/app/extra/page.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      page
+      <h1> only practice </h1>
+      <p>practicing contentfull api</p>
+    </div>
+  );
 };
 
 export default page;
