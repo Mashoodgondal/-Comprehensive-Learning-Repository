a560a3bf7303f11a43ce213d0abd74a87c8d7f5a
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-06 16:58:55 +0500

    contentfull practice

--- CMS-practice/app/extra/page.js
+++ CMS-practice/app/extra/page.js
@@ -1,50 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      <h1 className="text-indigo-500 text-center mt-10">
-        use for cms practice{" "}
-      </h1>
-      <p>this work with calling api and install library </p>
-      <h1 className="text-indigo-500 text-center mt-10">
-        use for cms practice{" "}
-      </h1>
-      <p>this work with calling api and install library </p>
-      <h1 className="text-indigo-500 text-center mt-10">
-        use for cms practice{" "}
-      </h1>
-      <p>this work with calling api and install library </p>
-      <h1 className="text-indigo-500 text-center mt-10">
-        use for cms practice{" "}
-      </h1>
-      <p>this work with calling api and install library </p>
-      <h1 className="text-indigo-500 text-center mt-10">
-        use for cms practice{" "}
-      </h1>
-      <p>this work with calling api and install library </p>
-      <h1 className="text-indigo-500 text-center mt-10">
-        use for cms practice{" "}
-      </h1>
-      <p>this work with calling api and install library </p>
-      <h1 className="text-indigo-500 text-center mt-10">
-        use for cms practice{" "}
-      </h1>
-      <p>this work with calling api and install library </p>
-      <h1 className="text-indigo-500 text-center mt-10">
-        use for cms practice{" "}
-      </h1>
-      <p>this work with calling api and install library </p>
-      <h1 className="text-indigo-500 text-center mt-10">
-        use for cms practice{" "}
-      </h1>
-      <p>this work with calling api and install library </p>
-      <h1 className="text-indigo-500 text-center mt-10">
-        use for cms practice{" "}
-      </h1>
-      <p>this work with calling api and install library </p>
-    </div>
-  );
+  return <div>page</div>;
 };
 
 export default page;
