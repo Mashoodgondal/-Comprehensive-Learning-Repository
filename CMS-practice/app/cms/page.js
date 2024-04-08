5eb6c675366d70b67c040bcabdc2d7b0b38188dc
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-08 22:43:37 +0500

    commit

--- CMS-practice/app/cms/page.js
+++ CMS-practice/app/cms/page.js
@@ -2,12 +2,13 @@ import client from "../lib/contentfulClient";
 
 const fetchServices = async () => {
   let responce = await client.getEntries({ content_type: "services" });
-  console.log(responce);
+
+  // console.log(responce);
 };
 
 const CmsPractice = async () => {
   const services = await fetchServices();
-  return <div></div>;
+  return <div> my page </div>;
 };
 
 export default CmsPractice;
