926b8d7ff3d2075fa168f38fc4f8e94b2c66402a
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-09 23:12:23 +0500

    commit

--- CMS-practice/app/cms/page.js
+++ CMS-practice/app/cms/page.js
@@ -20,15 +20,17 @@ const CmsPractice = async () => {
   return (
     <div>
       {" "}
-      <h1 className="text-3xl">Get Data from Contentful CMS </h1>
+      <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+      </h1>
       {services.map((service, i) => {
         return (
-          <>
+          <div className="text-center m-4 p-4 space-y-4 ">
             <h1 key={i}>{service.title}</h1>
             <h2>{service.description}</h2>
-            <img src={service.image} alt="" />
-            <hr />
-          </>
+            <img src={service.image} alt="" width={100} />
+            <hr className="h-2 " />
+          </div>
         );
       })}
     </div>
