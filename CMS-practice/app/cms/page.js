6436d065839803274be6299ebc72349abc89c1b5
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-09 17:28:26 +0500

    get data from contentful

--- CMS-practice/app/cms/page.js
+++ CMS-practice/app/cms/page.js
@@ -3,12 +3,36 @@ import client from "../lib/contentfulClient";
 const fetchServices = async () => {
   let responce = await client.getEntries({ content_type: "services" });
 
-  // console.log(responce);
+  const services = responce.items.map((item) => {
+    return {
+      title: item.fields.title,
+      description: item.fields.description,
+      image: item.fields.image.fields.file.url,
+    };
+  });
+  return services;
 };
 
 const CmsPractice = async () => {
   const services = await fetchServices();
-  return <div> my page </div>;
+  console.log(services);
+  // console.log(responce);
+  return (
+    <div>
+      {" "}
+      <h1 className="text-3xl">Get Data from Contentful CMS </h1>
+      {services.map((service, i) => {
+        return (
+          <>
+            <h1 key={i}>{service.title}</h1>
+            <h2>{service.description}</h2>
+            <img src={service.image} alt="" />
+            <hr />
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CmsPractice;
--- CMS-practice/app/lib/contentfulClient.js
+++ CMS-practice/app/lib/contentfulClient.js
@@ -1,7 +1,9 @@
 var contentful = require("contentful");
 import { space } from "postcss/lib/list";
 const client = contentful.createClient({
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-  space: process.env.CONTENTFUL_SPACE_ID,
+  // accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  // space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: "dy7uR_Ca5x9ObQm1zeGCZx4cPPR7gGEbXtK9c-fYe6g",
+  space: "8807elvegc64",
 });
 export default client;
