6ecf40c20a5f7abb99691511081beade428a9008
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-12 23:24:40 +0500

    commit

--- CMS-practice/app/aboutPage/page.js
+++ CMS-practice/app/aboutPage/page.js
@@ -1,13 +1,13 @@
 import client from "../lib/contentfulClient";
 
 const fetchAboutData = async () => {
-  let responce = await client.getEntries({ content_type: "About" });
-
+  let responce = await client.getEntry({ content_type: "About" });
+  // console.log(responce);
   const About = responce.items.map((item) => {
     return {
-      Name: item.fields.Name,
-      Address: item.fields.Address,
-      Description: item.fields.Description,
+      name: item.fields.name,
+      address: item.fields.address,
+      description: item.fields.description,
     };
   });
   return About;
@@ -18,12 +18,14 @@ const About = async () => {
   console.log(About);
   return (
     <div className="text-center">
+      <h1>Hellow World</h1>
       <h1>2nd entries</h1>
       {About.map((item) => {
         return (
-          (<h1>{item.Name}</h1>),
-          (<h2>{item.Address}</h2>),
-          (<h3>{item.Description}</h3>)
+          <div>
+            (<h1>{item.Name}</h1>), (<h2>{item.Address}</h2>), (
+            <h3>{item.Description}</h3>)
+          </div>
         );
         <hr />;
       })}
