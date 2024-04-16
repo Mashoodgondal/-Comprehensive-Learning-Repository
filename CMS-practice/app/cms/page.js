f0a7d72fd630092a8eab991bc1a8cc5abcf24204
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-16 22:42:29 +0500

    commit

--- CMS-practice/app/cms/page.js
+++ CMS-practice/app/cms/page.js
@@ -74,3 +74,23 @@ export default CmsPractice;
         Get Data from Contentful CMS{" "}
      </h1>*/
 }
+{
+  /* <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+      </h1>
+      <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+      </h1>
+      <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+      </h1> 
+       <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+         Get Data from Contentful CMS{" "}
+       </h1>
+      <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+         Get Data from Contentful CMS{" "}
+       </h1>
+       <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+     </h1>*/
+}
