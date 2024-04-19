b51460f89f7abb8cee61a0e6e11c86a9761e930c
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-19 22:51:31 +0500

    commit

--- CMS-practice/app/cms/page.js
+++ CMS-practice/app/cms/page.js
@@ -132,3 +132,39 @@ export default CmsPractice;
        <h1 className="text-3xl text-center text-indigo-500 font-bold ">
         Get Data from Contentful CMS{" "}
      </h1>*/
+/* <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+      </h1>
+      <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+      </h1>
+      <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+      </h1> 
+       <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+         Get Data from Contentful CMS{" "}
+       </h1>
+      <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+         Get Data from Contentful CMS{" "}
+       </h1>
+       <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+     </h1>*/
+/* <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+      </h1>
+      <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+      </h1>
+      <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+      </h1> 
+       <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+         Get Data from Contentful CMS{" "}
+       </h1>
+      <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+         Get Data from Contentful CMS{" "}
+       </h1>
+       <h1 className="text-3xl text-center text-indigo-500 font-bold ">
+        Get Data from Contentful CMS{" "}
+     </h1>*/
