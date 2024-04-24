ceeef882b1c76e908d86718b158988807369cb6b
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-24 21:11:03 +0500

    commit

--- To-do App/app/(components)/header/page.js
+++ To-do App/app/(components)/header/page.js
@@ -3,7 +3,7 @@ import React from "react";
 const Hader = () => {
   return (
     <div>
-      <header class="text-gray-600 body-font">
+      <header class="text-gray-600 body-font bg-gray-100 m-3 rounded-md">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <svg
