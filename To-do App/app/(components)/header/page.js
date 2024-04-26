e15ab2d71a8a5d69913b50c5b951cc8c57cf510c
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-04-26 23:08:55 +0500

    app

--- To-do App/app/(components)/header/page.js
+++ To-do App/app/(components)/header/page.js
@@ -28,6 +28,7 @@ const Hader = () => {
             <a class="mr-5 hover:text-blue-600 cursor-pointer">Fifth Link</a>
             <a class="mr-5 hover:text-blue-600 cursor-pointer">sixth Link</a>
             <a class="mr-5 hover:text-blue-600 cursor-pointer">seventh Link</a>
+            <a class="mr-5 hover:text-blue-600 cursor-pointer">seventh Link</a>
           </nav>
         </div>
       </header>
